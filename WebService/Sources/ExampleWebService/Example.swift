650445bcdeebd96659760a3fbea650be1030cb1c
Philipp Zagar <philipp.zagar@gmail.com>
2021-06-24 20:06:01 +0200

    Silence linter and finalize the workflow

--- WebService/Sources/ExampleWebService/Example.swift
+++ WebService/Sources/ExampleWebService/Example.swift
@@ -15,7 +15,7 @@ public struct Example: WebService {
     var port: Int = 8080
     
     @Flag
-    var revertDatabaseMigrations: Bool = false
+    var revertDatabaseMigrations = false
 
     private var databasePath: String {
         #if !DEBUG && os(Linux)
@@ -27,21 +27,21 @@ public struct Example: WebService {
     }
 
     public var configuration: Configuration {
-        /// Exposed interfaces, in this case a RESTful API and an OpenAPI documentation generated with it
+        // Exposed interfaces, in this case a RESTful API and an OpenAPI documentation generated with it
         REST {
             OpenAPI()
         }
         
-        /// Defines on which hostname and port the webservice should be bound to, configurable via CLI-arguments, else defaults
+        // Defines on which hostname and port the webservice should be bound to, configurable via CLI-arguments, else defaults
         HTTPConfiguration(hostname: hostname, port: port)
         
-        /// Setup of example database (in this case SQlite) and add migrations to create the respective tables
+        // Setup of example database (in this case SQlite) and add migrations to create the respective tables
         DatabaseConfiguration(.sqlite(.file(databasePath)))
             .addMigrations(ContactMigration())
             .addMigrations(ResidenceMigration())
         
-        /// If the appropriate CLI flag is passed, revert the database migrations
-        if(revertDatabaseMigrations) {
+        // If the appropriate CLI flag is passed, revert the database migrations
+        if revertDatabaseMigrations {
             DatabaseRevertConfiguration(.sqlite(.file(databasePath)))
                 .addMigrations(ContactMigration())
                 .addMigrations(ResidenceMigration())
