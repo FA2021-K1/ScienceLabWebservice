ce61cb9c2d11b0925c2b4e48195512a2e336704a
Mare0804 <63041075+Mare0804@users.noreply.github.com>
2021-09-22 10:55:39 +0200

    added grid layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import logo from "./logo.svg";
 import "./App.css";
 import LineChart from './lineChartComponent/LineChart';
@@ -10,6 +13,14 @@ import ListContainer from "./listComponent/list"
 import CardContainer from "./cardComponent/card"
 import reportWebVitals from './reportWebVitals'
 import Header from './headerComponent/Header.js'
+import Boxplot from "./boxplotComponent/boxplot";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const reactStyles = {
   position: "relative",
@@ -29,36 +40,45 @@ function App() {
 
   return (
     <React.StrictMode>
-    <Header />
-        <div>
-            <Grid container spacing={100}
-                justifyContent="space-evenly"
-            alignItems="center"            >
-                <Grid item md={5} xs={12}>
-                    <LineChart />
-                </Grid>
-                <Grid item md={3}>
-                    <Grid container spacing={100}
-                        direction="column"
-                        justifyContent="space-evenly"
-                        alignItems="center"            >
-                        <Grid item md={3}>
-                            <CardContainer />
-                        </Grid>
-                        <Grid item md={3}>
-                            <CardContainer />
-                        </Grid>
-                    </Grid>
-                    
-                </Grid>
-            </Grid>
-        </div>
-        
-        
-        <MapContainer />
-        <SliderContainer />
-        <ListContainer />
-        </React.StrictMode>
+      <Header />
+
+      <div>
+        <Grid container spacing={5}
+          justifyContent="space-evenly"
+          alignItems="center"
+          padding-left = {100}
+          padding-react = {100}>
+
+          <Grid item xs={12} xl={12}>
+            <Box sx={{ xs:12, height: '500', border: "1px solid red" }}>
+              <MapContainer />
+            </Box>
+          </Grid>
+
+          <Grid item xs={4} xl={4}>
+            <LineChart />
+          </Grid>
+          <Grid item xs={4} xl={4}>
+            <Boxplot />
+          </Grid>
+          <Grid item xs={4} xl={4}>
+            <Boxplot />
+          </Grid>
+
+          <Grid item xs={4} xl={4}>
+            <ListContainer />
+          </Grid>
+          <Grid item xs={8} xl={8}>
+            <LineChart />
+          </Grid>
+
+
+        </Grid>
+      </div>
+
+
+      
+    </React.StrictMode>
   );
 }
 
