8cafb760e0586b456e969d8cbb3b7c7b9f1a6ca7
Aaron Tacke <tacke@in.tum.de>
2021-09-23 13:24:21 +0200

    PAIN beendet - Query funktioniert

--- WebService/Sources/WebService/Components/MeasurementsHandler/GetAggregatedMeasurements.swift
+++ WebService/Sources/WebService/Components/MeasurementsHandler/GetAggregatedMeasurements.swift
@@ -67,23 +67,23 @@ struct GetAggregatedMeasurements: Handler {
         if let postgres = database as? PostgresDatabase {
             return try! await (postgres
             .simpleQuery("""
-            With Values ("buoyID", "date", "value") as
-            (
-            SELECT s."buoyID",
-            (TO_TIMESTAMP((cast(extract(epoch from TO_TIMESTAMP(m."measuredAt",'YYYY-MM-DD HH24:MI:SS')) as integer) / \(aggregationLevel) * \(aggregationLevel)) as date,
-            avg(d."value")
-            FROM "measurements" m
-                JOIN "measurementsData" d ON (m."measurementID" = d."measurementID")
-                JOIN "sensors" s             ON (d."sensorID" = s."sensorID")
-                JOIN "sensorTypes" t         ON (s."sensorTypeID" = t."sensorTypeID")
-            WHERE t."name" = '\(sensorTyp)' AND s."buoyID" = \(buoyID) AND m."measuredAt" BETWEEN '\(startDate.ISO8601Format(.init(dateSeparator: .dash, dateTimeSeparator: .space, timeZone: .current)))' AND '\(endDate.ISO8601Format(.init(dateSeparator: .dash, dateTimeSeparator: .space, timeZone: .current)))'
-            GROUP BY s."buoyID", date
-            )
-            SELECT v."buoyID", v."date", v."value",
-            (
-                SELECT m."coordinate" FROM "measurements" m WHERE m."buoyID" = v."buoyID" AND TO_TIMESTAMP(m."measuredAt",'YYYY-MM-DD HH24:MI:SS') >= v."date" ORDER BY m."measuredAt" ASC LIMIT 1
-            ) as position
-            FROM Values v;
+             With Values ("buoyID", "date", "value") as
+             (
+             SELECT s."buoyID",
+             (TO_TIMESTAMP((cast(extract(epoch from TO_TIMESTAMP(m."measuredAt",'YYYY-MM-DD HH24:MI:SS')) as integer) / \(aggregationLevel)) * \(aggregationLevel))) as date,
+             avg(d."value")
+             FROM "measurements" m
+                 JOIN "measurementsData" d ON (m."measurementID" = d."measurementID")
+                 JOIN "sensors" s             ON (d."sensorID" = s."sensorID")
+                 JOIN "sensorTypes" t         ON (s."sensorTypeID" = t."sensorTypeID")
+             WHERE t."name" = '\(sensorTyp)' AND s."buoyID" = \(buoyID) AND m."measuredAt" BETWEEN '\(startDate.ISO8601Format(.init(dateSeparator: .dash, dateTimeSeparator: .space, timeZone: .current)))' AND '\(endDate.ISO8601Format(.init(dateSeparator: .dash, dateTimeSeparator: .space, timeZone: .current)))'
+             GROUP BY s."buoyID", date
+             )
+             SELECT v."buoyID", v."date", v."value",
+             (
+                 SELECT m."coordinate" FROM "measurements" m WHERE m."buoyID" = v."buoyID" AND TO_TIMESTAMP(m."measuredAt",'YYYY-MM-DD HH24:MI:SS') >= v."date" ORDER BY m."measuredAt" ASC LIMIT 1
+             ) as position
+             FROM Values v;
             """)
             .map { rows in
                 rows.map { row in
@@ -96,4 +96,5 @@ struct GetAggregatedMeasurements: Handler {
         
         throw error
     }
+     
 }
