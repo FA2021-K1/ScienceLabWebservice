eebce5bb5f2352da064c12d85bace878ad8580cb
Viola Li <56644209+ViolaM151@users.noreply.github.com>
2021-09-26 16:20:19 +0200

    add gauge percent calculation

--- frontend/src/features/gauge/Gauge.js
+++ frontend/src/features/gauge/Gauge.js
@@ -1,9 +1,32 @@
 import GaugeChart from 'react-gauge-chart'
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from 'react';
 
 export const Gauge = () => {
     const style = useSelector((state) => state.style)
-    const data = useSelector(state => state.data.data)
+    const data = useSelector(state => state.data.latestData)
+
+    const [percentValue, setPercentValue] = useState(0);
+
+    useEffect(() => {
+        if (data) {
+            let list = [];
+            for (let key in data) {
+                list.push(data[key][0].value);
+            }
+            var sum = list.reduce(function (a, b) {
+                return a + b;
+            }, 0);
+            let percentValue = calculatePercent(sum);
+            setPercentValue(percentValue)
+
+        }
+    }, [data])
+
+    function calculatePercent(sum){
+        return sum / 400;
+    }
+
     return (
         <div>
 
@@ -16,7 +39,7 @@ export const Gauge = () => {
                 colors={[style.warningColor,
                 style.accentColor1,
                 style.lightGreen]}
-                percent={0.86} />
+                percent={percentValue} />
             <h4 align="left"> Latest measurement means of buoys</h4>
             <h5 align="left"> ph-Value: {data ? data['0'][0]["value"] : null} </h5>
             <h5 align="left"> TDS: {data ? data['0'][0]["value"] : null} ppm</h5>
