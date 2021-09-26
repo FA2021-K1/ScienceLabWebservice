f5325c3e4d719babd3bb838cc5bb2a86d3b28a86
Philipp, Zagar <philipp.zagar@gmail.com>
2021-09-26 17:16:08 +0200

    Fix auth

--- Shared/Sources/Shared/Models/User.swift
+++ Shared/Sources/Shared/Models/User.swift
@@ -37,10 +37,12 @@ public final class User: Model {
     }
     
     public func generateToken(signers: JWTSigners) throws -> Token {
-        let token = Token(expiration: .init(value: .init(timeIntervalSinceNow: 3600)),
-                          subject: .init(value: "apodini"),
-                          isAdmin: false,
-                          userID: try self.requireID())
+        let token = Token(
+            //expiration: .init(value: .init(timeIntervalSinceNow: 3600)),
+            //subject: .init(value: "apodini"),
+            isAdmin: false,
+            userID: try self.requireID()
+        )
         let jwt = try signers.sign(token)
         token.value = jwt
         return token
