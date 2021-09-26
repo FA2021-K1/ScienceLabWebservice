b9e7111a16472572964a9582be20408a0759d8a1
Felix Rinderer <rinderer@in.tum.de>
2021-09-26 17:14:23 +0200

    Remove unnecessary class name

--- frontend/src/features/export/Export.js
+++ frontend/src/features/export/Export.js
@@ -38,7 +38,7 @@ export const Export = () => {
   const data = jsonData.reduce((prev, curr) => prev.concat(getMeasurementData(curr)), []);
 
   if (data) {
-    return (<div className="export" style={{ height: 560, width: '100%', padding: 10, boxSizing: "border-box" }}>
+    return (<div style={{ height: 560, width: '100%', padding: 10, boxSizing: "border-box" }}>
       <DataGrid rows={data} columns={columns} components={{
         Toolbar: GridToolbar
       }} />
