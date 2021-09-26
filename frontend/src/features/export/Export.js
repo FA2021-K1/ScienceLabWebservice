bb041b186c35dc5d1720fe958857a012110d57f3
Felix Rinderer <rinderer@in.tum.de>
2021-09-26 11:17:21 +0200

    View raw (mock) data in grid including CSV export

--- frontend/src/features/export/Export.js
+++ frontend/src/features/export/Export.js
@@ -1,18 +1,49 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { getJsonData } from "../../dataSlice";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid"
+import { mockData } from "./mockData";
 
 export const Export = () => {
-  const dispatch = useDispatch();
-  const dataState = useSelector((state) => state.data.dataState);
-  const data = useSelector((state) => state.data.data);
+  const columns = [
+    {
+      field: 'measuredAt', headerName: 'Measured At', minWidth: 180, type: "date",
+      valueGetter: params => new Date(Date.parse(params.value)),
+      valueFormatter: params => `${params.value.toLocaleDateString()} ${params.value.toTimeString().split(' ')[0]}`
+    },
+    { field: 'buoyId', headerName: 'Buoy', type: 'number', minWidth: 110 },
+    {
+      field: 'location', headerName: 'Location', minWidth: 200, filterable: false, sortable: false,
+      valueGetter: params => `${params.getValue(params.id, 'longitude').toFixed(6)}, ${params.getValue(params.id, 'latitude').toFixed(6)}`
+    },
+    { field: 'sensor', headerName: 'Sensor', minWidth: 130, valueFormatter: params => "TODO" },
+    { field: 'value', headerName: 'Value', type: 'number', minWidth: 120 }
+  ];
 
-  useEffect(() => {
-    if (dataState === "idle") {
-      dispatch(getJsonData());
-    }
-  }, [dispatch, dataState]);
+  const jsonData = JSON.parse(mockData).data;
+  // make flat table out of measurements
+  const data = jsonData.reduce((prev, curr) => prev.concat(getMeasurementData(curr)), []);
 
-  return (<h2>Export</h2>);
+  if (data) {
+    return (<div style={{ height: 560, width: '100%', padding: 10, boxSizing: "border-box" }}>
+      <DataGrid rows={data} columns={columns} components={{
+        Toolbar: GridToolbar
+      }} />
+    </div>);
+  }
+  return (<div>Waiting for data ...</div>)
+}
+
+const getMeasurementData = measurement => {
+  const measuredAt = measurement.measuredAt;
+  const longitude = measurement.coordinate.longitude;
+  const latitude = measurement.coordinate.latitude;
+  const buoyId = measurement.buoyID;
+
+  return measurement.measurementData.map(data => ({
+      id: data.id,
+      measuredAt: measuredAt,
+      longitude: longitude,
+      latitude: latitude,
+      buoyId: buoyId,
+      sensorId: data.sensor.id,
+      value: data.value
+  }));
 }
---
+++ frontend/src/features/export/mockData.js
@@ -0,0 +1,34 @@
+export const mockData = `{
+  "data": [
+    {
+      "updatedAt": "2021-09-26",
+      "createdAt": "2021-09-26",
+      "measurementData": [
+        {
+          "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+          "sensor": {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+          },
+          "measurement": {
+            "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+          },
+          "value": 0,
+          "createdAt": "2021-09-26",
+          "updatedAt": "2021-09-26"
+        }
+      ],
+      "buoyID": 0,
+      "measuredAt": "2021-09-26",
+      "coordinate": {
+        "longitude": 0,
+        "latitude": 0
+      },
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    }
+  ],
+  "_links": {
+    "additionalProp1": "string",
+    "additionalProp2": "string",
+    "additionalProp3": "string"
+  }
+}`
