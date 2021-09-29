25b136fb736035e570d7d6de6ddd8f515e442faf
Mare0804 <63041075+Mare0804@users.noreply.github.com>
2021-09-29 13:22:56 +0200

    Update ColumnChart.js

--- frontend/src/features/columnChart/ColumnChart.js
+++ frontend/src/features/columnChart/ColumnChart.js
@@ -3,92 +3,122 @@ import ReactApexChart from "react-apexcharts";
 import { useSelector } from "react-redux";
 
 export const ColumnChart = () => {
-  const style = useSelector((state) => state.style);
-  const data = useSelector((state) => state.data.latestData);
 
+    const style = useSelector((state) => state.style);
+    const data = useSelector((state) => state.data.latestData);
 
-  const [series, setSeries] = useState([]);
-
-  const [options, setOptions] = useState({
-    chart: {
-      type: "bar",
-      height: 350,
-    },
-    colors: [style.pH, style.TDS],
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "55%",
-        endingShape: "rounded",
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      show: true,
-      width: 2,
-      colors: ["transparent"],
-    },
-    xaxis: {
-      categories: ["Bouy 1", "Bouy 2", "Bouy 3"],
-    },
-    yaxis: [
-      {
-        title: {
-          text: "pH Value [-]",
-        },
-      },
-      {
-        opposite: true,
-        title: {
-          text: "Dissolved Solids [ppm]",
-        },
-      },
-    ],
-    title: {
-      text: "Latest measurements per buoy",
-      align: "left",
-      style: {
-        color: style.textColor,
-      }
-    },
-fill: {
-  opacity: 1,
-    },
-  });
-
-useEffect(() => {
-  if (data) {
-    let list = [];
-    for (let key in data) {
-      list.push(key);
+    function roundToOne(num) {
+        return +(Math.round(num + "e+1") + "e-1")
     }
-    setOptions({ ...options, xaxis: { categories: list } });
-    setSeries([
-      {
-        name: "pH-Value",
-        data: list.map((element) => data[element]["0"].value),
-      },
-      {
-        me: "TDS-Value",
-        data: list.map((element) => data[element]["1"].value),
-      }
 
-      // TODO: Add other Sensor Values accordingly
-    ]);
-  }
-}, [data]);
+    const [series, setSeries] = useState([]);
+
+    const [options, setOptions] = useState({
+        chart: {
+            type: "bar",
+            height: 350,
+        },
+        colors: [style.pH, style.TDS],
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: "55%",
+                endingShape: "rounded",
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ["transparent"],
+        },
+        xaxis: {
+            categories: ["Bouy 1", "Bouy 2", "Bouy 3"],
+        },
+        yaxis: [
+            {
+                min: undefined,
+                max: undefined,
+                title: {
+                    text: "pH Value [-]",
+                },
+                labels: {
+                    formatter: (value) => {
+                      return Math.round(value);
+                    },
+                  },
+            },
+            {
+                opposite: true,
+                min: undefined,
+                max: undefined,
+                title: {
+                    text: "Dissolved Solids [ppm]",
+                },
+                labels: {
+                    formatter: (value) => {
+                      return Math.round(value);
+                    },
+                  },
+            },
+        ],
+        tooltip: {
+            y: [{
+                formatter: function(value) {
+                    return roundToOne(value)
+                }
+            },
+            {
+                formatter: function(value) {
+                return value
+            }}
+            ]
+        },
+        title: {
+            text: "Latest measurements per buoy",
+            align: "left",
+            style: {
+                color: style.textColor,
+            }
+        },
+        fill: {
+            opacity: 1,
+        },
+    });
+
+    useEffect(() => {
+        if (data) {
+            let list = [];
+            for (let key in data) {
+                list.push(key);
+            }
+            setSeries([
+                {
+                    name: "pH-Value",
+                    data: list.map((element) => data[element]["0"].value),
+                },
+                {
+                    name: "TDS-Value",
+                    data: list.map((element) => data[element]["1"].value),
+                }
+
+                // TODO: Add other Sensor Values accordingly
+            ]);
+            setOptions({ ...options, xaxis: { categories: list }});
+        }
+    }, [data]);
 
 
-return (
-  <div id="chart">
-    <ReactApexChart
-      options={options}
-      series={series}
-      type="bar"
-      height={350}
-    />
-  </div>
-);
+    return (
+        <div id="chart">
+            <ReactApexChart
+                options={options}
+                series={series}
+                type="bar"
+                height={350}
+            />
+        </div>
+    );
 };
