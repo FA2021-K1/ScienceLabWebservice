1b2314ddfac21fc5de91c615e41e949ba49f32ac
Felix Rinderer <rinderer@in.tum.de>
2021-09-29 12:54:53 +0200

    Use real data for csv export

--- frontend/src/features/export/Export.js
+++ frontend/src/features/export/Export.js
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
-import { DataGrid, GridToolbar as MuiGridToolbar } from "@mui/x-data-grid"
+import { useEffect, useState } from "react";
+import { DataGrid, GridToolbar as MuiGridToolbar, GridToolbarContainer, GridToolbarColumnsButton, GridToolbarFilterButton, GridToolbarDensitySelector, GridToolbarExport, GridToolbar } from "@mui/x-data-grid"
 import { mockData } from "./mockData";
 import { styled } from '@mui/material/styles';
+import { DateRangePicker } from "materialui-daterange-picker";
+import DateRangeIcon from "@material-ui/icons/DateRange";
+import IconButton from '@mui/material/IconButton';
 
 import { useDispatch, useSelector } from "react-redux";
 
 export const Export = () => {
   const dispatch = useDispatch();
   const style = useSelector((state) => state.style);
+  const latestData = useSelector((state) => state.data.latestData);
   useEffect(() => { }, [dispatch, style]);
+
+  const [open, setOpen] = useState(false);
+  let startDate = new Date();
+  startDate.setMonth(startDate.getMonth() - 1);
+  // console.log(startDate);
+  const [dateRange, setDateRange] = useState({
+    startDate: startDate,
+    endDate: new Date()
+  });
 
   const GridToolbar = styled(MuiGridToolbar)(
     ({ theme }) => ({
@@ -18,11 +31,43 @@ export const Export = () => {
     })
   );
 
+  const CustomizedGridToolbar = () => {
+    return (
+      <div>
+        <GridToolbarContainer>
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+            <div>
+              From <b>{dateToString(dateRange.startDate)}</b> to <b>{dateToString(dateRange.endDate)}</b>
+            </div>
+            <div>
+              <IconButton aria-label="Pick date range" onClick={toggle}>
+                <DateRangeIcon />
+              </IconButton>
+            </div>
+            <div>
+              <GridToolbar />
+            </div>
+          </div>
+        </GridToolbarContainer>
+        <div className="date-range-picker">
+          <DateRangePicker
+            open={open}
+            toggle={toggle}
+            onChange={(range) => setDateRange(range)}
+            wrapperClassName="date-range-picker"
+          />
+        </div>
+      </div>
+    );
+  }
+
+  const dateToString = date => `${date.toLocaleDateString()} ${date.toLocaleTimeString().split(' ')[0]}`
+
   const columns = [
     {
-      field: 'measuredAt', headerName: 'Measured At', minWidth: 180, type: "date",
+      field: 'measuredAt', headerName: 'Measured At', minWidth: 200, type: "date",
       valueGetter: params => new Date(Date.parse(params.value)),
-      valueFormatter: params => `${params.value.toLocaleDateString()} ${params.value.toTimeString().split(' ')[0]}`
+      valueFormatter: params => dateToString(params.value)
     },
     { field: 'buoyId', headerName: 'Buoy', type: 'number', minWidth: 110 },
     {
@@ -33,15 +78,28 @@ export const Export = () => {
     { field: 'value', headerName: 'Value', type: 'number', minWidth: 120 }
   ];
 
-  const jsonData = JSON.parse(mockData).data;
-  // make flat table out of measurements
-  const data = jsonData.reduce((prev, curr) => prev.concat(getMeasurementData(curr)), []);
+  const data = null;
+
+  useEffect(() => {
+    if (latestData) {
+      console.log(latestData);
+      data = latestData.reduce((prev, curr) => prev.concat(getMeasurementData(curr)), []);
+    }
+  }, [dispatch, latestData])
+
+  const toggle = () => setOpen(!open);
 
   if (data) {
     return (<div style={{ height: 560, width: '100%', padding: 10, boxSizing: "border-box" }}>
       <DataGrid rows={data} columns={columns} components={{
-        Toolbar: GridToolbar
-      }} />
+        Toolbar: CustomizedGridToolbar
+      }} onFilterModelChange={e => console.log(e)}
+        filterModel={{
+          items: [
+            { id: '1', columnField: 'measuredAt', operatorValue: 'onOrBefore', value: `${dateRange.startDate.getFullYear()}-${dateRange.startDate.getMonth()}-${dateRange.startDate.getDate()}` },
+            // { id: '2', columnField: 'measuredAt', operatorValue: 'onOrBefore', value: `${dateRange.startDate.getFullYear()}-${dateRange.startDate.getMonth()}-${dateRange.startDate.getDate()}` }
+          ]
+        }} />
     </div>);
   }
   return (<div>Waiting for data ...</div>)
