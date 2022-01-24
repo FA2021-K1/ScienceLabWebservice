330274f647cbd19301e58ed8dd9a9cb134219415
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-01-24 09:39:27 +0100

    Update to Apodini 0.8.0

--- WebService/Package.swift
+++ WebService/Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .executable(name: "WebService", targets: ["WebService"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.7.1")),
-        .package(url: "https://github.com/Apodini/ApodiniObservePrometheus.git", .upToNextMinor(from: "0.1.2")),
+        .package(url: "https://github.com/Apodini/Apodini.git", .upToNextMinor(from: "0.8.0")),
+        .package(url: "https://github.com/Apodini/ApodiniObservePrometheus.git", .upToNextMinor(from: "0.1.3")),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/Apodini/swift-log-elk.git", from: "0.2.0"),
         .package(url: "https://github.com/vapor-community/bcrypt.git", from: "1.1.0"),
