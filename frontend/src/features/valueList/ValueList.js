02920c5ed874c928c6edbba096d877414d7ab887
Philipp Zagar <philipp.zagar@gmail.com>
2022-02-02 01:11:04 +0100

    Fix frontend data visualization

--- frontend/src/features/valueList/ValueList.js
+++ frontend/src/features/valueList/ValueList.js
@@ -18,59 +18,63 @@ export const ValueList = () => {
   const dateFormatter = (time) => {
     return format(time, "MMM dd HH:mm:ss");
   }
-  
+
   const renderRow = ({ index, style }) => {
-    let color = "white"
-    let sensor;
-    if (data[index].sensorTypeID === 0) {
-      sensor = "pH-Value"
-      if (data[index].value <= 5.5 || data[index].value >= 8.5) {
-        color = styleColor.warningColorLight;
+    if (index < data.length) {
+      let color = "white"
+      let sensor;
+      if (data[index].sensorTypeID === 0) {
+        sensor = "pH-Value"
+        if (data[index].value <= 5.5 || data[index].value >= 8.5) {
+          color = styleColor.warningColorLight;
+        }
       }
-    }
-    if (data[index].sensorTypeID === 1) {
-      sensor = "TDS-Value"
-      if (data[index].value >= 700) {
-        color = styleColor.warningColorLight;
+      if (data[index].sensorTypeID === 1) {
+        sensor = "TDS-Value"
+        if (data[index].value >= 700) {
+          color = styleColor.warningColorLight;
+        }
       }
+
+      return (
+        <ListItem key={index} style={style} component="div" disablePadding>
+          <Box sx={{ width: "100%", height: "100%", bgcolor: color }}>
+            <ListItem>
+              <ListItemText
+                primary={sensor}
+                primaryTypographyProps={{
+                  color: styleColor.textColor,
+                  fontSize: '12px'
+                }}
+                secondary={`Buoy ${data[index].buoyID}`}
+                secondaryTypographyProps={{
+                  color: styleColor.textColor,
+                  fontSize: '12px'
+                }}
+              />
+
+              <ListItemText
+                align="right"
+                primary={data[index].value}
+                primaryTypographyProps={{
+                  color: styleColor.textColor,
+                  fontSize: '12px'
+                }}
+                secondary={dateFormatter(new Date(data[index].date))}
+                secondaryTypographyProps={{
+                  color: styleColor.textColor,
+                  fontSize: '12px'
+                }}
+              />
+            </ListItem>
+          </Box>
+
+
+        </ListItem>
+      );
+    } else {
+      return (null);
     }
-    
-    return (
-      <ListItem key={index} style={style} component="div" disablePadding>
-        <Box sx={{ width: "100%", height: "100%", bgcolor:color}}>
-          <ListItem>
-            <ListItemText 
-              primary={sensor}
-              primaryTypographyProps={{
-                color: styleColor.textColor,
-                fontSize: '12px'
-              }}
-              secondary={`Buoy ${data[index].buoyID}`}
-              secondaryTypographyProps={{
-                color: styleColor.textColor,
-                fontSize: '12px'
-              }}
-            />
-
-            <ListItemText
-              align="right"
-              primary={data[index].value}
-              primaryTypographyProps={{
-                color: styleColor.textColor,
-                fontSize: '12px'
-              }}
-              secondary={dateFormatter(new Date(data[index].date))}
-              secondaryTypographyProps={{
-                color: styleColor.textColor,
-                fontSize: '12px'
-              }}
-            />
-          </ListItem>
-        </Box>
-
-
-      </ListItem>
-    );
   };
 
   return (
@@ -82,9 +86,9 @@ export const ValueList = () => {
           </div>
           <div id="more-data-button">
             <Link to="/export">
-            <Button style={{ fontSize: "12px", backgroundColor: styleColor.primaryColor}} variant="contained" href="">
-                  More data
-                </Button>
+              <Button style={{ fontSize: "12px", backgroundColor: styleColor.primaryColor }} variant="contained" href="">
+                More data
+              </Button>
             </Link>
           </div>
         </div>
